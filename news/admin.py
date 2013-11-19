10bf29939b993e8966a0f296c25d3daf19b39a55
zdimon <zdimon77@gmail.com>
2013-11-20 01:29:11 +0200

    image

--- news/admin.py
+++ news/admin.py
@@ -2,9 +2,10 @@
 from django.contrib import admin
 from news.models import News
 from news.models import NewsImages
+from sorl.thumbnail.admin import AdminImageMixin
 
 
-class NewsImagesInline(admin.TabularInline):
+class NewsImagesInline(AdminImageMixin, admin.TabularInline):
     model = NewsImages
     verbose_name_plural = u'Изображения'
 
--- news/models.py
+++ news/models.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from tinymce import models as tinymce_models
-from news.items import ThumbnailImageField
+from sorl.thumbnail import get_thumbnail
+
 
 # Create your models here.
 class News(models.Model):
@@ -9,11 +10,20 @@ class News(models.Model):
     image  = models.ImageField(upload_to='news', verbose_name=u'Изображение', blank=True)
     desc = models.TextField(verbose_name=u'Короткое описание')
     content = tinymce_models.HTMLField()
-    datetime = models.DateTimeField(u'Дата публикации')
+    datetime = models.DateField(verbose_name=u'Дата публикации')
     def __unicode__(self):
         return self.title
     def get_absolute_url(self):
         return u"/новость/%i/" % self.id
+
+    @property
+    def parsecontent(self):
+        c = unicode(self.content)
+        for i in self.newsimages_set.all():
+            im = get_thumbnail(i.image.path, '683', crop='center', quality=99)
+            istr = '<a  class="fancybox" rel="group" href="'+i.image.url+'"><img src="'+im.url+'" /></a>'
+            c = c.replace(i.alias, istr)
+        return c
     class Meta:
         verbose_name_plural = u'Новости'
         verbose_name = u'Новость'
@@ -23,8 +33,8 @@ class News(models.Model):
 class NewsImages(models.Model):
     news = models.ForeignKey('News')
     title = models.CharField(max_length=250, verbose_name=u'Заголовок')
-    image  = ThumbnailImageField(upload_to='recipe',verbose_name=u'Изображение')
-    alias = models.CharField(max_length=250, verbose_name=u'Метка')
+    image  = models.ImageField(upload_to='news', verbose_name=u'Изображение')
+    alias = models.CharField(max_length=250, verbose_name=u'Метка', help_text=u'Строка которая будет заменена в тексте изображением')
     def __unicode__(self):
         return self.title
     class Meta:
